42be2c513b886a3420edaeb7f539a240c81903bc
promz <promz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-15 10:01:19 +0000

    updated scheduling options and upload image options
    git-svn-id: https://plugins.svn.wordpress.org/facebook-publish/trunk@1007812 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 1.1/fp_activate_plugin.php
@@ -0,0 +1,40 @@
+<?php
+		
+	global $fp_settings;
+	global $fp_version;
+	
+	if(!$fp_settings) {
+
+		$fp_settings = get_option("fp_settings");
+
+		//remove all old settings if outdated version
+		if(!isset($fp_settings["version"])) {
+			$fp_settings = array();			
+		}
+
+		$defaults = array(
+							'version' 				=> $fp_version,
+							'ptf_app_id' 			=> "",
+							'ptf_api_key' 			=> "",
+							'fp_post_types' 		=> array("post" , "page"),
+							'fp_post_cat' 			=> array("Uncategorized"),
+							'fp_all_posts' 			=> "0",
+							'show_auth_btn' 		=> true,
+							'msg_body'				=> "My new post on {SITE_NAME}", 
+							'post_featured_image'	=> "featured", 
+							'post_schedule' 		=> "no",
+							'queue' 				=> array(),
+							'session_data' 			=> "",
+							'pages' 				=> array(), 
+							'global_pages' 			=> array( "own"=> array("id" => "own" , "name" => "Own Timeline")  ), 
+							'automatic_posts' 		=> "no",
+						);
+
+		$fp_settings = wp_parse_args( $fp_settings, $defaults );
+
+	}
+
+	update_option( "fp_settings", $fp_settings );
+
+	//exit;
+
---
+++ 1.1/lib/class/hybridauth/config.php
@@ -0,0 +1,152 @@
+<?php
+
+/*!
+
+* HybridAuth
+
+* http://hybridauth.sourceforge.net | http://github.com/hybridauth/hybridauth
+
+* (c) 2009-2012, HybridAuth authors | http://hybridauth.sourceforge.net/licenses.html
+
+*/
+
+
+
+// ----------------------------------------------------------------------------------------
+
+//	HybridAuth Config file: http://hybridauth.sourceforge.net/userguide/Configuration.html
+
+// ----------------------------------------------------------------------------------------
+
+global $fp_settings;
+
+//pre($fp_settings);
+
+
+
+return 
+
+	array(
+
+		"base_url" => site_url( "/wp-content/plugins/facebook-publish/lib/class/hybridauth/" ), 
+
+
+
+		"providers" => array ( 
+
+			// openid providers
+
+			"OpenID" => array (
+
+				"enabled" => true
+
+			),
+
+
+
+			"Yahoo" => array ( 
+
+				"enabled" => true,
+
+				"keys"    => array ( "id" => "", "secret" => "" ),
+
+			),
+
+
+
+			"AOL"  => array ( 
+
+				"enabled" => true 
+
+			),
+
+
+
+			"Google" => array ( 
+
+				"enabled" => true,
+
+				"keys"    => array ( "id" => "", "secret" => "" ), 
+
+			),
+
+
+
+			"Facebook" => array ( 
+
+				"enabled" => true,
+
+				"keys"    => array ( "id"		=> isset($fp_settings["ptf_app_id"])?$fp_settings["ptf_app_id"]:"",
+
+									 "secret" 	=> isset($fp_settings["ptf_api_key"])?$fp_settings["ptf_api_key"]:"" ),
+
+				"scope" => "email, user_about_me, publish_actions, manage_pages"  
+
+			),
+
+
+
+			"Twitter" => array ( 
+
+				"enabled" => true,
+
+				"keys"    => array ( "key" => "", "secret" => "" ) 
+
+			),
+
+
+
+			// windows live
+
+			"Live" => array ( 
+
+				"enabled" => true,
+
+				"keys"    => array ( "id" => "", "secret" => "" ) 
+
+			),
+
+
+
+			"MySpace" => array ( 
+
+				"enabled" => true,
+
+				"keys"    => array ( "key" => "", "secret" => "" ) 
+
+			),
+
+
+
+			"LinkedIn" => array ( 
+
+				"enabled" => true,
+
+				"keys"    => array ( "key" => "", "secret" => "" ) 
+
+			),
+
+
+
+			"Foursquare" => array (
+
+				"enabled" => true,
+
+				"keys"    => array ( "id" => "", "secret" => "" ) 
+
+			),
+
+		),
+
+
+
+		// if you want to enable logging, set 'debug_mode' to true  then provide a writable file by the web server on "debug_file"
+
+		"debug_mode" => false,
+
+
+
+		"debug_file" => "logger.txt",
+
+	);
+
