3c70aa9d3d3109963228263e67282045fdf57e46
promz <promz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-20 19:47:24 +0000

    eliminated issues related with hybridauth error logs
    git-svn-id: https://plugins.svn.wordpress.org/facebook-publish/tags/1.3.1@1095395 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/admin/add-settings-page-link.php
@@ -0,0 +1,22 @@
+<?php
+
+add_filter('plugin_action_links', 'fp_plugin_action_links', 10, 2);
+
+function fp_plugin_action_links($links, $file) {
+    static $this_plugin;
+
+    if (!$this_plugin) {
+        $this_plugin = "facebook-publish/init.php";
+    }
+    
+    if ($file == $this_plugin) {
+        // The "page" query string value must be equal to the slug
+        // of the Settings admin page we defined earlier, which in
+        // this case equals "myplugin-settings".
+        $settings_link  = '<a href="' . get_bloginfo('wpurl') . '/wp-admin/admin.php?page=facebook-publish">Settings</a>';
+        array_unshift($links, $settings_link);
+
+    }
+
+    return $links;
+}
